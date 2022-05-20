9c7a1a939c5b955651b2194ddd176fe763c2d59a
Natalia Sereda <natalia.sereda.ex@gmail.com>
2022-05-20 15:42:39 +0300

    add new page : home content

--- src/components/BodyContent/MainContent/MainContent.jsx
+++ src/components/BodyContent/MainContent/MainContent.jsx
@@ -5,6 +5,7 @@ import { Profile } from "../../../pages/LayoutPages/Profile/Profile";
 import { Box } from "../../Box/Box";
 import { DisplayCardSmallBlock } from "../../DIsplayCard/DisplayCardSmallBlock";
 import { PayCard } from "../../PayCard/PayCard";
+import { Card } from "../../Card/Card";
 import Styles from "./MainContent.module.scss";
 
 export const MainContent = () => {
@@ -15,6 +16,7 @@ export const MainContent = () => {
       views: 1234,
       price: 234,
       img: "https://mebel-v-sibiri.ru/files/originals/stolik-kofeynyy-sr-0559-s-143542.jpg",
+      isLike: true,
     },
     {
       id: 34,
@@ -22,6 +24,7 @@ export const MainContent = () => {
       views: 348,
       price: 343,
       img: "https://www.hdretail.ru/upload/iblock/160/160fa37cea09204ec381995fd90d405e.jpg",
+      isLike: true,
     },
     {
       id: 54,
@@ -29,6 +32,7 @@ export const MainContent = () => {
       views: 9328,
       price: 23,
       img: "https://remobi.ru/media/img/models_small/remont-watch/nika/nika-0362-0-1-46.jpg",
+      isLike: false,
     },
     {
       id: 52,
@@ -36,6 +40,7 @@ export const MainContent = () => {
       views: 45,
       price: 227,
       img: "https://main-cdn.sbermegamarket.ru/big2/hlr-system/-51/085/182/511/222/54/100029639788b0.jpg",
+      isLike: false,
     },
     {
       id: 69,
@@ -43,6 +48,7 @@ export const MainContent = () => {
       views: 14356,
       price: 93,
       img: "https://main-cdn.sbermegamarket.ru/big2/hlr-system/317/364/883/113/100/100029647525b0.jpg",
+      isLike: false,
     },
     {
       id: 57,
@@ -50,13 +56,26 @@ export const MainContent = () => {
       views: 590,
       price: 26,
       img: "https://ae01.alicdn.com/kf/Hee2bf11f6af1428cb6181d35433b3841i.jpg",
+      isLike: false,
     },
   ]);
 
   return (
     <div className={Styles.content_magazine}>
       <Routes>
-        <Route path="/home/main" element={<div>MAIN</div>} />
+        <Route
+          path="/home/main"
+          element={
+            <Box color={"#f7f5f5"}>
+              <div className={Styles.content_cards}>
+                {products?.length &&
+                  products?.map((element) => (
+                    <Card key={element.id} card={element} />
+                  ))}
+              </div>
+            </Box>
+          }
+        />
         <Route path="/home/women" element={<div>WOMEN</div>} />
         <Route path="/home/men" element={<div>MEN</div>} />
         <Route path="/home/best" element={<div>BEST SELLER</div>} />
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,44 @@
+import clsx from "clsx";
+import useSound from "use-sound";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import StarIcon from "@mui/icons-material/Star";
+import likeHeart from "../../sounds/like.mp3";
 import Styles from "./Card.module.scss";
 
-export const Card = () => {
-  return <></>;
+export const Card = ({ card }) => {
+  const [play] = useSound(likeHeart);
+  const { title, isLike, img, views, price } = card;
+
+  const handleClickHeart = () => {
+    play();
+    console.log("fine");
+  };
+
+  const handleClickProduct = () => {
+    console.log("---lalala");
+  };
+
+  return (
+    <div className={Styles.cards}>
+      <div
+        className={clsx(Styles.icon, isLike && Styles.icon_isLike)}
+        onClick={handleClickHeart}
+      >
+        <FavoriteIcon fontSize="small" className={Styles.heart} />
+      </div>
+      <div className={Styles.block_content} onClick={handleClickProduct}>
+        <div className={Styles.img_product}>
+          <img src={img} alt="card" className={Styles.img_product}></img>
+        </div>
+        <div className={Styles.block_text}>
+          <span className={Styles.title}>{title}</span>
+          <div className={Styles.icon_star}>
+            <StarIcon fontSize="10px" className={Styles.icon_icon} />
+            <span className={Styles.text}>({views} reviews)</span>
+          </div>
+          <span className={Styles.price}>$ {price}.00</span>
+        </div>
+      </div>
+    </div>
+  );
 };
